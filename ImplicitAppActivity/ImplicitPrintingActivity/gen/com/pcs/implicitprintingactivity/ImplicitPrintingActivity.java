459ab54f0dc1416fc408580f7a796f0578bc8da4
Dharma237 <dharmasai.seerapu@paradigmcreatives.com>
2014-09-11 11:58:15 +0530

    Java Files Created

---
+++ ImplicitAppActivity/ImplicitPrintingActivity/gen/com/pcs/implicitprintingactivity/ImplicitPrintingActivity.java
@@ -0,0 +1,15 @@
+package com.pcs.implicitprintingactivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ImplicitPrintingActivity extends Activity{
+	private TextView printTxt;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+	}
+
+}
