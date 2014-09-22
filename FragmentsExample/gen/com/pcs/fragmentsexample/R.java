874e8f7c8f4102fa4e305df11f78086793fd3084
Dharma237 <dharmasai.seerapu@paradigmcreatives.com>
2014-09-22 18:28:52 +0530

    FragmentsExample Completed

--- FragmentsExample/gen/com/pcs/fragmentsexample/R.java
+++ FragmentsExample/gen/com/pcs/fragmentsexample/R.java
@@ -18,8 +18,9 @@ public final class R {
     }
     public static final class id {
         public static final int android_btn=0x7f060000;
-        public static final int fragment=0x7f060003;
+        public static final int fragment=0x7f060004;
         public static final int ios_btn=0x7f060001;
+        public static final int list=0x7f060003;
         public static final int web_btn=0x7f060002;
     }
     public static final class layout {
