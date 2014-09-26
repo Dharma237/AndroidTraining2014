87c1141fc66def4af249d0f5374f9fa9728cb13d
Dharma237 <dharmasai.seerapu@paradigmcreatives.com>
2014-09-26 18:30:17 +0530

    MapsActivity is completed With Documentation

--- MapsActivity/src/com/pcs/mapsactivity/MainActivity.java
+++ MapsActivity/src/com/pcs/mapsactivity/MainActivity.java
@@ -1,6 +1,12 @@
 package com.pcs.mapsactivity;
 
-import com.google.android.gms.maps.CameraUpdate;
+
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -9,43 +15,58 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import android.app.Activity;
-import android.os.Bundle;
-
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 public class MainActivity extends Activity{
 	private GoogleMap googleMap;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		
 		setContentView(R.layout.activity_main);
 
-
+		try {
+			//loads map
 		intializeMap();
+		
+		//setting MapType
 		googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-
+		
+		//displaying our location
 		googleMap.setMyLocationEnabled(true);
-
+		
+		//setting zoom controls
 		googleMap.getUiSettings().setZoomControlsEnabled(false);
-
+		
+		//mylocation Button
 		googleMap.getUiSettings().setMyLocationButtonEnabled(true);
-
+		
+		//Enabling Compass
 		googleMap.getUiSettings().setCompassEnabled(true);
-
+		
+		//Enabling Gestures
 		googleMap.getUiSettings().setAllGesturesEnabled(true);
-
+		
+		//Enabling Zoom Controls
 		googleMap.getUiSettings().setZoomControlsEnabled(true);
-
+		
+		/***
+		 * Defining Latitudes and longitude
+		 */
 		double latitude = 20.087;
 		double longitude = 77.0345;
-
+		
 		for (int i=0; i<5; i++)
-		{
+		{	
+			//Setting our RandomLocation
 			double[] randomLocation = createRandomLocation(latitude,longitude);
 			MarkerOptions markerOptions = new MarkerOptions().
+					
 					position(new LatLng(randomLocation[0],randomLocation[1]))
+					
 					.title(getResources().getString(R.string.marker_txt));
+			
+			
 			if (i == 0)
 				markerOptions.icon(BitmapDescriptorFactory
 						.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
@@ -64,7 +85,10 @@ public class MainActivity extends Activity{
 			if (i == 5)
 				markerOptions.icon(BitmapDescriptorFactory
 						.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE));
+			
 			googleMap.addMarker(markerOptions);
+			
+			//moving camera to the last zoon
 			if(i==5)
 			{
 				CameraPosition cameraPosition = new CameraPosition
@@ -76,17 +100,24 @@ public class MainActivity extends Activity{
 						.newCameraPosition(cameraPosition));
 			}
 		}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 	}
-
+	//Initializing our GoogleMap
+	@SuppressLint("NewApi")
 	private void intializeMap() {
+		
 		if (googleMap==null) {
-			googleMap= (((MapFragment) getFragmentManager().findFragmentById(R.id.map_fragment)).getMap());
+			googleMap= (((MapFragment) getFragmentManager()
+					.findFragmentById(R.id.map_fragment)).getMap());
 			
 			
 		}
 
 	}
+	//Creating RandomLocation using Latitude and Longitude
 	private double[] createRandomLocation(double latitude, double longitude) {
 		
 		return new double[]{ latitude + ((Math.random() - 0.5) / 500),
