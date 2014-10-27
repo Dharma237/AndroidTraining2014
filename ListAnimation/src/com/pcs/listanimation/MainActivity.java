3a0074e45d311054004de3a1db1d9945775b3f95
Dharma237 <dharmasai.seerapu@paradigmcreatives.com>
2014-10-27 16:46:21 +0530

    ListAnimation is completed

--- ListAnimation/src/com/pcs/listanimation/MainActivity.java
+++ ListAnimation/src/com/pcs/listanimation/MainActivity.java
@@ -9,25 +9,31 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
- 
+
 public class MainActivity extends ListActivity {
-	
-	//activity starts with onCreate() method
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        
-        setContentView(R.layout.activity_main);
-        
-       
-        
-        String[] mStrings = getResources().getStringArray(R.array.technologies);
-        
-        //Array List to the Technologies Array
-        setListAdapter(new ArrayAdapter<String>(this,
-        		android.R.layout.simple_list_item_1, new ArrayList<String>(Arrays.asList(mStrings))));
-    }
- 
-    
+
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+
+		setContentView(R.layout.activity_main);
+
+		getListItems();
+
+	}
+
+	private void getListItems() {
+		String[] mStrings = getResources().getStringArray(R.array.technologies);
+
+		//Array List to the Technologies Array
+		setListAdapter(new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1,
+				new ArrayList<String>(Arrays.asList(mStrings))));
+
+	}
+
+
 }
