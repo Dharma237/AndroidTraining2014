66e2d33703ce4ae1a4893f3b9b9f86961374afa5
Dharma237 <dharmasai.seerapu@paradigmcreatives.com>
2014-10-27 15:32:35 +0530

    Animations ShadeInOut, ZoomInOut, Roatation, Shanke are completed

--- AnimationsOnView/gen/com/pcs/animationsonview/R.java
+++ AnimationsOnView/gen/com/pcs/animationsonview/R.java
@@ -8,16 +8,36 @@
 package com.pcs.animationsonview;
 
 public final class R {
+    public static final class anim {
+        public static final int fade_animation=0x7f040000;
+        public static final int rotate_animation=0x7f040001;
+        public static final int translate_animation=0x7f040002;
+        public static final int zoom_animation=0x7f040003;
+    }
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int animation=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int sky=0x7f020002;
+    }
+    public static final class id {
+        public static final int animate_img=0x7f070005;
+        public static final int fadeInOut_btn=0x7f070003;
+        public static final int layout=0x7f070000;
+        public static final int rotate_btn=0x7f070004;
+        public static final int test=0x7f070001;
+        public static final int zoomInOut_btn=0x7f070002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
+        public static final int app_name=0x7f050000;
+        public static final int fade_btn=0x7f050002;
+        public static final int image=0x7f050004;
+        public static final int rotate_btn=0x7f050003;
+        public static final int zoom_btn=0x7f050001;
     }
     public static final class style {
         /** 
@@ -41,10 +61,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
--- AnimationsOnView/src/com/pcs/animationsonview/MainActivity.java
+++ AnimationsOnView/src/com/pcs/animationsonview/MainActivity.java
@@ -2,14 +2,66 @@ package com.pcs.animationsonview;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends Activity{
-	
-	
+public class MainActivity extends Activity implements OnClickListener{
+
+	private Button fadeInOut_Btn,zoomInOut_Btn,rotate_Btn;
+	private ImageView animate_Img;
+
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-	setContentView(R.layout.activity_main);	
+
+
+		setContentView(R.layout.activity_main);
+
+		fadeInOut_Btn = (Button)findViewById(R.id.fadeInOut_btn);
+		zoomInOut_Btn = (Button)findViewById(R.id.zoomInOut_btn);
+		rotate_Btn = (Button)findViewById(R.id.rotate_btn);
+		animate_Img = (ImageView)findViewById(R.id.animate_img);
+
+		//onClick Listeners to the Buttons
+		fadeInOut_Btn.setOnClickListener(this);
+		zoomInOut_Btn.setOnClickListener(this);
+		rotate_Btn.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+
+		/***
+		 * FadeInOut Animation
+		 * 
+		 */
+		case R.id.fadeInOut_btn:
+
+			Animation fade_animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_animation);
+			animate_Img.startAnimation(fade_animation);
+			break;
+
+		/***
+		* ZoomInOut Animation
+		* 
+		*/	
+		case R.id.zoomInOut_btn:
+
+			Animation zoom_animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.zoom_animation);
+			animate_Img.startAnimation(zoom_animation);
+			break;
+
+		case R.id.rotate_btn:
+
+			Animation rotate_animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.translate_animation);
+			animate_Img.startAnimation(rotate_animation);
+			break;
+		}
+
 	}
 }
