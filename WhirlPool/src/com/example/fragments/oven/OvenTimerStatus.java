281697ece42949983039bb49181312fd9469de35
Dharma237 <dharmasai.seerapu@paradigmcreatives.com>
2014-11-04 17:11:53 +0530

    WhirlPool Activity is completed

---
+++ WhirlPool/src/com/example/fragments/oven/OvenTimerStatus.java
@@ -0,0 +1,18 @@
+package com.example.fragments.oven;
+
+import com.example.whirlpool.R;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class OvenTimerStatus extends Fragment{
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.oven_timer_status, container);
+	}
+}
